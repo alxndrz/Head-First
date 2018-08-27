3203f1871bf84d2cd6dada20f6a1e88e2817f697
Alexandr Zherdev <rndm4n@outlook.com>
2018-08-28 02:35:56 +0600

    Add method .chomp for gets

--- get_number.rb
+++ get_number.rb
@@ -2,6 +2,10 @@
 # Written by: Me!
 
 puts "Welcome to 'Get My Number!'"
+
+# Get the player's name and show the greeting.
 print "What's your name? "
-input = gets
-puts "Welcome, #{input}"
+input = gets.chomp
+puts "Welcome, #{input}!"
+
+# Save a random number.
