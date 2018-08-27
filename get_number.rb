49a9679709a1f26e4502ba222ee363e4ef9a8aba
Alexandr Zherdev <rndm4n@outlook.com>
2018-08-28 03:01:04 +0600

    Add validation
    Comparison of the entered number with the concealed and output of the corresponding message. If there is no attempt, tell the number.

--- get_number.rb
+++ get_number.rb
@@ -5,8 +5,8 @@ puts "Welcome to 'Get My Number!'"
 
 # Get the player's name and show the greeting.
 print "What's your name? "
-input = gets.chomp
-puts "Welcome, #{input}!"
+name = gets.chomp
+puts "Welcome, #{name}!"
 
 # Save a random number.
 puts "I've got a random number between 1 and 100"
@@ -15,4 +15,28 @@ target = rand(100) + 1
 
 # Track the number of attempts.
 num_guesses = 0
+
+# A sign of the continuation of the game.
+guessed_it = false
+
 puts "You've got #{10 - num_guesses} guesses left."
+print "Make a guess: "
+guess = gets.to_i
+
+# Comparison of the entered number with the concealed 
+# and output of the corresponding message.
+if guess < target
+	puts "Oops. Your guess was LOW."
+elsif guess > target
+	puts "Oops. Your guess was HIGH."
+elsif guess == target
+	puts "Good job, #{name}!"
+	puts "You guessed my number in #{num_guesses} guesses!"
+	guessed_it = true
+end
+
+# If there is no attempt, tell the number.
+if not guessed_it
+	puts "Sorry. You didn't get my number. It was #{target}."
+end
+		
