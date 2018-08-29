76e0e912dad7c0c4a2eceb7838349d4abf9c087a
Alexandr Zherdev <rndm4n@outlook.com>
2018-08-30 01:39:42 +0600

    optimization code

--- get_number.rb
+++ get_number.rb
@@ -4,14 +4,14 @@
 puts "Welcome to 'Get My Number!'"
 
 # Get the player's name and show the greeting.
-print "What's your name? "
+print "What's your name?"
 name = gets.chomp
 puts "Welcome, #{name}!"
 
 # Save a random number.
 puts "I've got a random number between 1 and 100"
-puts "Can you guess it?"
-target = rand(100) + 1
+puts 'Can you guess it?'
+target = rand(1..100)
 
 # Track the number of attempts.
 num_guesses = 0
@@ -21,7 +21,7 @@ guessed_it = false
 
 until num_guesses == 10 || guessed_it
   puts "You've got #{10 - num_guesses} guesses left."
-  print "Make a guess: "
+  print 'Make a guess: '
   guess = gets.to_i
 
   num_guesses += 1
@@ -29,9 +29,9 @@ until num_guesses == 10 || guessed_it
   # Comparison of the entered number with the concealed
   # and output of the corresponding message.
   if guess < target
-    puts "Oops. Your guess was LOW."
+    puts 'Oops. Your guess was LOW.'
   elsif guess > target
-    puts "Oops. Your guess was HIGH."
+    puts 'Oops. Your guess was HIGH.'
   elsif guess == target
     puts "Good job, #{name}!"
     puts "You guessed my number in #{num_guesses} guesses!"
@@ -40,6 +40,4 @@ until num_guesses == 10 || guessed_it
 end
 
 # If there is no attempt, tell the number.
-unless guessed_it
-  puts "Sorry. You didn't get my number. It was #{target}."
-end
+puts "Sorry. You didn't get my number. It was #{target}." unless guessed_it
